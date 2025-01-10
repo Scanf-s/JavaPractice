4cb22f378cb5e581521d382fc2190792b05e7105
Scanf-s <calzone0404@gmail.com>
2025-01-11 02:10:00 +0900

    Basic MVC annotation usages

---
+++ SpringMVC/src/main/java/com/sullung2yo/springmvc/basic/requestmapping/ModelMappingController.java
@@ -0,0 +1,18 @@
+package com.sullung2yo.springmvc.basic.requestmapping;
+
+import com.sullung2yo.springmvc.basic.dto.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ModelMappingController {
+
+    @GetMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+}
