8a9ae7a7b03dea0626a1b6e288aa744a0729d4e5
Scanf-s <calzone0404@gmail.com>
2025-01-31 00:09:55 +0900

    Configuration files for WebSocket and RabbitMQ

--- RabbitMQ/src/main/java/com/sullung2yo/rabbitmq/pub_sub_realtime_websocket_alarm/RabbitMQConfig.java
+++ RabbitMQ/src/main/java/com/sullung2yo/rabbitmq/pub_sub_realtime_websocket_alarm/RabbitMQConfig.java
@@ -1,5 +1,7 @@
 package com.sullung2yo.rabbitmq.pub_sub_realtime_websocket_alarm;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
@@ -20,5 +22,11 @@ public class RabbitMQConfig {
     public FanoutExchange fanoutExchange() {
         // 모든 큐로 작업 전송 (Fanout Exchange)
         return new FanoutExchange(FANOUT_EXCHANGE);
+    }
+
+    @Bean
+    public Binding bindNotification(Queue notificationQueue, FanoutExchange fanoutExchange) {
+        // Fanout Exchange와 큐를 연결(Binding)
+        return BindingBuilder.bind(notificationQueue).to(fanoutExchange);
     }
 }
